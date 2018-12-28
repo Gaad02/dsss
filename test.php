837a8bde62da5b1e39b1483a7db5963f015a2541
Gaad02 <45686360+Gaad02@users.noreply.github.com>
2018-12-28 14:23:33 +0500

    Начал разбираться с phpQuery

--- test.php
+++ test.php
@@ -1,84 +1,81 @@
 <?php
-$data = $_GET;
-if (!isset($data['nameFile'])) {
+if (isset($_REQUEST['buttonName'])) {
+  require_once('phpQuery/phpQuery.php');
 
-  function file_error($number = 3)
+  function curlGetContents($page_url, $base_url, $pause_time, $retry)
   {
-    switch ($number) {
-      case '1':
-        echo 'Файл отсутстует в директории';
-        break;
+      /*
+      $page_url - адрес страницы-источника
+      $base_url - адрес страницы для поля REFERER
+      $pause_time - пауза между попытками парсинга
+      $retry - 0 - не повторять запрос, 1 - повторить запрос при неудаче
+      */
+      $error_page = [];
+      $ch = curl_init();
 
-      case '2':
-       echo 'Херня с файлом происходит<br>';
-        break;
 
-      case '3':
-        echo '404 :)<br>';
-        break;
+      curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0");
+      curl_setopt($ch, CURLOPT_COOKIEJAR, str_replace("\\", "/", getcwd()).'/gearbest.txt');
+      curl_setopt($ch, CURLOPT_COOKIEFILE, str_replace("\\", "/", getcwd()).'/gearbest.txt');
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // Автоматом идём по редиректам
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Не проверять SSL сертификат
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // Не проверять Host SSL сертификата
+      curl_setopt($ch, CURLOPT_URL, $page_url);          // Куда отправляем
+      curl_setopt($ch, CURLOPT_REFERER, $base_url);      // Откуда пришли
+      curl_setopt($ch, CURLOPT_HEADER, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Возвращаем, но не выводим на экран результат
 
-    }
-  }
+      $response['html'] = curl_exec($ch);
 
-  function includeFiles($arrayNames = [])
-  {
-    if (!empty($arrayNames)) {
-      foreach ($arrayNames as $key => $value) {
-        if (file_exists("$value")) {
-          checkFile("$value");
-        } else {
-          file_error(1);
-        }
+      $info = curl_getinfo($ch);
+      if($info['http_code'] != 200 && $info['http_code'] != 404) {
+          $error_page[] = array(1, $page_url, $info['http_code']);
+          if($retry) {
+              sleep($pause_time);
+              $response['html'] = curl_exec($ch);
+              $info = curl_getinfo($ch);
+              if($info['http_code'] != 200 && $info['http_code'] != 404)
+                  $error_page[] = array(2, $page_url, $info['http_code']);
+          }
       }
-    } else {
-      return 0;
-    }
+      $response['code'] = $info['http_code'];
+      $response['errors'] = $error_page;
+      curl_close($ch);
+      return $response;
+
   }
 
-  function checkFile($nameFile)
-  {
-    if (!empty($nameFile)) {
-      $contentFile = file_get_contents("$nameFile");
-      if (empty($contentFile)) {
-        echo 'Файл - '.$nameFile.' <b>пустой</b><br>';
-      } else {
-        echo 'Файл - '.$nameFile.' - '.filesize("$nameFile").' (Байта)<br>';
-      }
-    } else {
-      return 0;
-    }
+  $link = $_REQUEST['link'];
+
+  $str = curlGetContents($link, 'google.com', 0, 0);
+  $objeckPage = phpQuery::newDocument($str['html']);
+  switch ($_REQUEST['elems']) {
+    case 'title':
+      $elems  = $str->find('title');
+      break;
+
+    case 'body':
+          $elems  = $str->find('body');
+        break;
   }
-
-  $arrayNames = ['work1.php', 'work2.txt', 'work3.php'];
-  includeFiles($arrayNames);
-
-
 } else {
-  if (!file_exists('dir')) {
-    mkdir('dir');
-  }
-
-  $nameFile = $data['nameFile'].'.php';
-  var_dump($nameFile);
-  file_put_contents("dir/$nameFile", ' ');
-  if (file_exists("$nameFile")) {
-    echo 'Создание прошло успешно';
-  }
+  ?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  	<meta charset="UTF-8">
+  	<title>Тестовая html страница</title>
+  </head>
+  <body>
+  	<form action="#" method="POST">
+      Ссылка: <input type="text" name="link">
+      <select name="elems">
+        <option value="title">title</option>
+        <option value="body">body</option>
+      </select>
+  		<input type="submit" name="buttomName">
+  	</form>
+  </body>
+  </html>
+  <?
 }
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <form action="index.php" method="get">
-    <input type="text" name="nameFile">
-    <input type="submit" name="nameButtom" value="Создать файл">
-  </form>
-</body>
-</html>
